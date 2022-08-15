c04dd008c7fb88db65c38cbc2c55aea24b8c15de
hanna 🍌 <hanna@iamhanna.de>
2022-08-15 20:39:00 +0200

    Include logging for prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -12,10 +12,14 @@ let prisma: PrismaClient;
 
 if (typeof window === 'undefined') {
   if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
+    prisma = new PrismaClient({
+      log: ['query', 'info', 'warn', 'error'],
+    });
   } else {
     if (!global.prisma) {
-      global.prisma = new PrismaClient();
+      global.prisma = new PrismaClient({
+        log: ['query', 'info', 'warn', 'error'],
+      });
     }
 
     prisma = global.prisma;
