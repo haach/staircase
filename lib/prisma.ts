5efd18dccf85cc4f4bf0c7f2f82aa5dbb0ce9daa
hanna 🍌 <hanna@iamhanna.de>
2022-08-15 21:18:53 +0200

    Remove extensive logging for prisma client in prod exclude warnings for annanymous default exports

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -12,9 +12,7 @@ let prisma: PrismaClient;
 
 if (typeof window === 'undefined') {
   if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient({
-      log: ['query', 'info', 'warn', 'error'],
-    });
+    prisma = new PrismaClient();
   } else {
     if (!global.prisma) {
       global.prisma = new PrismaClient({
