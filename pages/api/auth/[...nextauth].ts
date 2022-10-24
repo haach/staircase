682155b07e33d19b33032b00f2d538c73dabd09a
haach <hanna@iamhanna.de>
2022-10-24 16:31:01 +0200

    Add email provider, temp removed signin

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import {NextApiHandler} from 'next';
 import NextAuth, {NextAuthOptions} from 'next-auth';
 import {PrismaAdapter} from '@next-auth/prisma-adapter';
 import GitHubProvider from 'next-auth/providers/github';
+import EmailProvider from 'next-auth/providers/email';
 import prisma from '../../../lib/prisma';
 
 const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options);
@@ -13,11 +14,15 @@ const options: NextAuthOptions = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
   ],
   adapter: PrismaAdapter(prisma),
   secret: process.env.SECRET,
-  pages: {
+  /*  pages: {
     signIn: '/signin',
-  },
+  }, */
   // debug: true,
 };
