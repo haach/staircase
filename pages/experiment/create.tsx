b766e42913a9053dd2726c11509a779bf590aa62
haach <hanna@iamhanna.de>
2022-10-24 17:34:32 +0200

    Refactor experiment create

--- pages/experiment/create.tsx
+++ pages/experiment/create.tsx
@@ -5,59 +5,9 @@ import {useRouter} from 'next/router';
 import React from 'react';
 
 const createExperiment = async (experiment) => {
-  // Create groups with mice
-  const groups = await Promise.all(
-    experiment.groups.map(async (group) => {
-      // create Mice
-      const mice = await Promise.all(
-        group.mice.map(async (mouse) => {
-          const createMouseBody: Prisma.MouseCreateArgs = {
-            data: {
-              ...mouse,
-              groupId: group.id,
-            },
-          };
-          const createMouse = await fetch('/api/mouse/create', {
-            method: 'POST',
-            body: JSON.stringify(createMouseBody),
-          });
-          return await createMouse.json();
-        })
-      );
-
-      const createGroupBody: Prisma.GroupCreateArgs = {
-        data: {
-          ...group,
-          mice: {
-            connect: mice.map(({id}) => ({id})),
-          },
-        },
-        include: {
-          mice: true,
-        },
-      };
-
-      const createGroup = await fetch('/api/group/create', {
-        method: 'POST',
-        body: JSON.stringify(createGroupBody),
-      });
-
-      return await createGroup.json();
-    })
-  );
-  // Create experiment and connect previouslycreated groups
-  const createExperimentBody: Prisma.ExperimentCreateArgs = {
-    data: {
-      name: experiment.name,
-      displayId: experiment.displayId,
-      groups: {
-        connect: groups.map(({id}) => ({id})),
-      },
-    },
-  };
   const createExperiment = await fetch('/api/experiment/create', {
     method: 'POST',
-    body: JSON.stringify(createExperimentBody),
+    body: JSON.stringify(experiment),
   });
   if (!createExperiment.ok) {
     throw new Error('Error creating experiment');
