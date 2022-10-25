3268dd4e29706dc8beb44bb1be1cc338e601a384
haach <hanna@iamhanna.de>
2022-10-25 16:28:08 +0200

    Refactor experiment update

--- pages/api/experiment/update.ts
+++ pages/api/experiment/update.ts
@@ -1,22 +1,39 @@
 import {Prisma} from '@prisma/client';
 import prisma from 'lib/prisma';
 import {NextApiRequest, NextApiResponse} from 'next';
+import {Experiment} from 'types';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'POST') {
     return res.status(405).json({message: 'Method not allowed'});
   }
-  const experiment: Prisma.ExperimentUpdateInput = JSON.parse(req.body);
+
+  const experiment: Experiment = JSON.parse(req.body);
   if (!experiment?.id) {
     return res.status(400).json({message: 'Invalid Input'});
   }
 
   const prismaExperimentUpdateArgs: Prisma.ExperimentUpdateArgs = {
-    where: {
-      id: experiment.id as string,
-    },
+    where: {id: experiment.id as string},
     data: {
-      ...experiment,
+      name: experiment.name,
+      displayId: experiment.displayId,
+      groups: {
+        deleteMany: {},
+        connectOrCreate: experiment.groups.map((group) => ({
+          where: {id: group.id ?? ''},
+          create: {
+            ...group,
+            experimentId: undefined,
+            mice: {
+              connectOrCreate: group.mice.map((mouse) => ({
+                where: {id: mouse.id ?? ''},
+                create: {...mouse, groupId: undefined},
+              })),
+            },
+          },
+        })),
+      },
     },
   };
 
