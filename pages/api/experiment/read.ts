d902192fcc691342f5ba1423e9c21fca7bc443ef
haach <hanna@iamhanna.de>
2022-10-25 13:23:52 +0200

    Refactor experiment read

--- pages/api/experiment/read.ts
+++ pages/api/experiment/read.ts
@@ -1,3 +1,4 @@
+import {Prisma} from '@prisma/client';
 import prisma from 'lib/prisma';
 import {NextApiRequest, NextApiResponse} from 'next';
 
@@ -5,6 +6,21 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'POST') {
     return res.status(405).json({message: 'Method not allowed'});
   }
-  const readExperiment = await prisma.experiment.findUnique(JSON.parse(req.body));
+
+  const id: string = req.body;
+  const prismaExperimentFindUniqueArgs: Prisma.ExperimentFindUniqueArgs = {
+    where: {
+      id,
+    },
+    include: {
+      groups: {
+        include: {
+          mice: true,
+        },
+      },
+    },
+  };
+
+  const readExperiment = await prisma.experiment.findUnique(prismaExperimentFindUniqueArgs);
   res.json(readExperiment);
 };
--- pages/api/experiment/readMany.ts
+++ pages/api/experiment/readMany.ts
@@ -1,10 +1,41 @@
+import {Prisma} from '@prisma/client';
 import prisma from 'lib/prisma';
 import {NextApiRequest, NextApiResponse} from 'next';
+
+export const prismaExperimentFindManyArgs: Prisma.ExperimentFindManyArgs = {
+  orderBy: [
+    {
+      updatedAt: 'desc',
+    },
+    {
+      createdAt: 'desc',
+    },
+  ],
+  include: {
+    groups: {
+      orderBy: [
+        {
+          groupNumber: 'asc',
+        },
+      ],
+      include: {
+        mice: {
+          orderBy: [
+            {
+              mouseNumber: 'asc',
+            },
+          ],
+        },
+      },
+    },
+    recordingSessions: true,
+  },
+};
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'POST') {
     return res.status(405).json({message: 'Method not allowed'});
   }
-  const readManyExperiments = await prisma.experiment.findMany(JSON.parse(req.body));
+  const readManyExperiments = await prisma.experiment.findMany(prismaExperimentFindManyArgs);
   res.json(readManyExperiments);
 };
