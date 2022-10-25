5ef502ca535460fba38b2cbabac58c8739b71efd
haach <hanna@iamhanna.de>
2022-10-25 15:16:14 +0200

    Refactor recordingSession delete

--- pages/api/recordingSession/delete.ts
+++ pages/api/recordingSession/delete.ts
@@ -1,3 +1,4 @@
+import {Prisma} from '@prisma/client';
 import prisma from 'lib/prisma';
 import {NextApiRequest, NextApiResponse} from 'next';
 
@@ -5,6 +6,9 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'POST') {
     return res.status(405).json({message: 'Method not allowed'});
   }
-  const deletedRecordingSession = await prisma.recordingSession.delete(JSON.parse(req.body));
+  const prismaRecordingSessionDeleteArgs: Prisma.RecordingSessionDeleteArgs = {
+    where: {id: req.body as string},
+  };
+  const deletedRecordingSession = await prisma.recordingSession.delete(prismaRecordingSessionDeleteArgs);
   res.json(deletedRecordingSession);
 };
