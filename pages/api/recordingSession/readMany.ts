c5df6b93ea169acba0783bbb6b58ef0640677e2a
haach <hanna@iamhanna.de>
2022-10-25 15:16:02 +0200

    Refactor recordingSession readMany

--- pages/api/recordingSession/readMany.ts
+++ pages/api/recordingSession/readMany.ts
@@ -1,3 +1,4 @@
+import {Prisma} from '@prisma/client';
 import prisma from 'lib/prisma';
 import {NextApiRequest, NextApiResponse} from 'next';
 
@@ -5,6 +6,32 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'POST') {
     return res.status(405).json({message: 'Method not allowed'});
   }
-  const readManyRecordingSessions = await prisma.recordingSession.findMany(JSON.parse(req.body));
+
+  const body = JSON.parse(req.body);
+  const experimentId = body.experimentId;
+
+  const prismaRecordingSessionFindManyArg: Prisma.RecordingSessionFindManyArgs = {
+    include: {
+      author: true,
+      runs: true,
+    },
+    orderBy: [
+      {
+        updatedAt: 'desc',
+      },
+      {
+        createdAt: 'desc',
+      },
+    ],
+  };
+
+  if (experimentId) {
+    // Get by experiment id
+    prismaRecordingSessionFindManyArg.where = {
+      experimentId,
+    };
+  }
+
+  const readManyRecordingSessions = await prisma.recordingSession.findMany(prismaRecordingSessionFindManyArg);
   res.json(readManyRecordingSessions);
 };
