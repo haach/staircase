80a69f25e3ed4933a1d9a11603f4242898a46fd2
haach <hanna@iamhanna.de>
2022-10-27 18:08:58 +0200

    Add enpoint for recursive group deletion

--- pages/api/group/delete.ts
+++ pages/api/group/delete.ts
@@ -1,41 +1,41 @@
 import prisma from 'lib/prisma';
 import {NextApiRequest, NextApiResponse} from 'next';
-import {RecordingSession} from 'types';
+import {Group} from 'types';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'POST') {
     return res.status(405).json({message: 'Method not allowed'});
   }
 
-  const id: RecordingSession['id'] = req.body;
+  const id: Group['id'] = req.body;
   if (!id) {
     return res.status(400).json({message: 'Invalid Input'});
   }
 
-  const nestedRuns = await prisma.run.findMany({
+  const nestedMice = await prisma.mouse.findMany({
     where: {
-      recordingSessionId: id,
+      groupId: id,
     },
     select: {
       id: true,
     },
   });
 
-  if (nestedRuns.length > 0) {
-    const deleteRuns = prisma.run.deleteMany({
+  if (nestedMice.length > 0) {
+    const deleteMice = prisma.run.deleteMany({
       where: {
-        id: {in: nestedRuns.map((run) => run.id)},
+        id: {in: nestedMice.map((run) => run.id)},
       },
     });
-    const deleteRecordingSession = prisma.recordingSession.delete({
+    const deleteGroup = prisma.group.delete({
       where: {id},
     });
 
-    const result = await prisma.$transaction([deleteRuns, deleteRecordingSession]);
+    const result = await prisma.$transaction([deleteMice, deleteGroup]);
     res.json(result);
   }
 
-  const result = await prisma.recordingSession.delete({
+  const result = await prisma.group.delete({
     where: {id},
   });
 
